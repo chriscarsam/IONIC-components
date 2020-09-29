ac1f3d2b6dc422e9d5cc37a1626df4800a5f55c6
chriscar <chriscar.sam@yahoo.com>
2020-09-29 16:55:40 -0500

    ion-datetime Parte 1

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -43,6 +43,11 @@ export class InicioPage implements OnInit {
       icon: 'checkmark-circle',
       name: 'Checks',
       redirectTo: '/check'
+    },
+    {
+      icon: 'calendar',
+      name: 'Datetime',
+      redirectTo: '/date-time'
     }
   ]
 
