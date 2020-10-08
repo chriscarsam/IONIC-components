616fdf59729af53dc9865f54798e188a85eb5ef5
chriscar <chriscar.sam@yahoo.com>
2020-10-08 17:35:56 -0500

    ion-list

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -68,6 +68,11 @@ export class InicioPage implements OnInit {
       icon: 'hammer',
       name: 'Input Forms',
       redirectTo: '/input'
+    },
+    {
+      icon: 'list',
+      name: 'List - Sliding',
+      redirectTo: '/list'
     }
   ]
 
