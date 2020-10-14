094f1af5b0ce5ca82dfb96778236db06e57b5bad
chriscar <chriscar.sam@yahoo.com>
2020-10-14 17:23:49 -0500

    ion-menu - Parte 2 - opciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { Platform } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DataService } from './services/data.service';
+import { Componente } from './interfaces/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent{
+
+  componentes: Observable<Componente[]>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
   }
-
+ 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.componentes = this.dataService.getMenuOpts();
     });
   }
 }
